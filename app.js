f6e99abb5ee9dbd3d907bf948ace821f70f92a75
thenameisahalya <ahalyavl24@gmail.com>
2024-06-11 22:51:46 +0530

    project done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const {coursemodel}=require("./models/course")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://ahalyalinson:ahalya@cluster0.5hcgbes.mongodb.net/courseDB?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+   let input = req.body
+   let course=new coursemodel(input)
+   course.save()
+   console.log(course)
+   res.json({"status":"success"})
+})
+app.get("/view",(req,res) => {
+    coursemodel.find().then(
+
+    (data)=>{
+        res.json(data)
+    }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+            
+        })
+
+
+app.listen(8084)
+console.log("server started")
